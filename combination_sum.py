c0d5bbff999b624a70081f71a966bed193ca1171
santoshnaruje <santoshnaruje@gmail.com>
2026-02-25 09:19:55 +0530

    Tested with time complexity of target is bigger and smallest
    Time complexity is 2^(T/M)
    
    T is the taret
    M is the minumum of candidates or nums

--- combination_sum.py
+++ combination_sum.py
@@ -17,7 +17,10 @@ class Solution:
 
 if __name__ == '__main__':
     solution = Solution()
-    nums = [2, 3, 6, 7]
-    target = 7
-    current_sum = 0
+    # nums = [2, 3, 6, 7]
+    # target = 7
+
+    nums = [1,2]
+    target = 50
+
     print(solution.get_combination(nums, 0, target,[]))
