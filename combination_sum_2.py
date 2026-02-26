6e17610fc5ec718b20cb4d033f30332286ba0cac
santoshnaruje <santoshnaruje@gmail.com>
2026-02-26 09:27:07 +0530

    Solved - Combination using backtrack for loops

--- combination_sum_2.py
+++ combination_sum_2.py
@@ -1,6 +1,6 @@
 class Solution:
-    def get_combination(self, nums,target):
-
+    def get_combination(self, nums, target):
+        nums.sort()
         result = []
 
         def get_backtrack(index, remaining, path):
@@ -13,15 +13,21 @@ class Solution:
                 return
 
             path.append(nums[index])
-            get_backtrack(index, remaining - nums[index], path)
+            get_backtrack(index + 1, remaining - nums[index], path)
             path.pop()
-            get_backtrack(index + 1, remaining, path)
+
+            next_index = index + 1
+
+            while next_index < len(nums) and nums[next_index] == nums[index]:
+                next_index += 1
+
+            get_backtrack(next_index, remaining, path)
 
         get_backtrack(0, target, [])
         return result
 
-    def get_combination_using_for_loop_backtrack(self, nums, target):
-
+    def get_combination_using_for_loop_backtrack(self,nums,remaining):
+        nums.sort()
         result = []
 
         def get_backtrack(start, remaining, path):
@@ -34,21 +40,22 @@ class Solution:
                 return
 
             for i in range(start, len(nums)):
+                if i > start and nums[i-1] == nums[i]:
+                    continue
                 path.append(nums[i])
-                get_backtrack(i, remaining - nums[i], path)
+                get_backtrack(i+1, remaining - nums[i], path)
                 path.pop()
 
-        get_backtrack(0, target, [])
+        get_backtrack(0, remaining, [])
         return result
 
 
 if __name__ == '__main__':
     solution = Solution()
-    nums = [2, 3, 6, 7]
-    target = 7
+    nums = [10, 1, 2, 7, 6, 1, 5]
+    target = 8
     #
     # nums = [1, 2]
     # target = 50
 
-    print(solution.get_combination(nums, target))
     print(solution.get_combination_using_for_loop_backtrack(nums, target))
